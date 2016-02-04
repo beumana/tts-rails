32fa974cfac0111c8f2af4dced326260ba714a06
Bryant Umana <bryantumana@gmail.com>
2016-02-04 13:03:04 +0000

    added comment scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :comments
   resources :blog_posts
   root 'blog_posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
@@ -36,8 +38,7 @@ Rails.application.routes.draw do
 
   # Example resource route with more complex sub-resources:
   #   resources :products do
-  #     resources :comments
-  #     resources :sales do
+  #     #     resources :sales do
   #       get 'recent', on: :collection
   #     end
   #   end
